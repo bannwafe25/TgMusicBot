e871f464070702118d29f7d5cf26f05d0a044291
zp <inizulpii25@gmail.com>
2026-08-17 02:30:43 +0800

    Translate messages to Indonesian

--- src/handlers/play.go
+++ src/handlers/play.go
@@ -137,7 +137,7 @@ func handlePlay(c *td.Client, m *td.Message, isVideo bool, force bool) error {
 		return td.EndGroups
 	}
 
-	updater, err := m.ReplyText(c, "🔍 Searching and downloading...", nil)
+	updater, err := m.ReplyText(c, "🔍 Sabar ajg...", nil)
 	if err != nil {
 		c.Logger.Warn("failed to send message", "error", err)
 		return td.EndGroups
@@ -222,7 +222,7 @@ func handleMedia(c *td.Client, m *td.Message, updater *td.Message, dlMsg *td.Mes
 		escName := html.EscapeString(saveCache.Name)
 		escUser := html.EscapeString(saveCache.User)
 		queueInfo := fmt.Sprintf(
-			"<u><b>Added to queue: %d</b></u>\n\n<b>Title:</b> <a href='%s'>%s</a>\n\n<b>Duration:</b> %s min\n<b>Requested by:</b> %s",
+			"<u><b>Tambah ke antrian: %d</b></u>\n\n<b>Judul:</b> <a href='%s'>%s</a>\n<b>Durasi:</b> %s min\n<b>Req by:</b> %s",
 			qLen, escURL, escName, utils.SecToMin(saveCache.Duration), escUser,
 		)
 		_, err := updater.EditText(c, queueInfo, &td.EditTextMessageOpts{ReplyMarkup: core.QueueMarkup(saveCache.TrackID), ParseMode: "HTML", DisableWebPagePreview: true})
@@ -255,7 +255,7 @@ func handleMedia(c *td.Client, m *td.Message, updater *td.Message, dlMsg *td.Mes
 	escUser := html.EscapeString(saveCache.User)
 
 	nowPlaying := fmt.Sprintf(
-		"<u><b>| Started streaming</b></u>\n\n<b>Title:</b> <a href='%s'>%s</a>\n\n<b>Duration:</b> %s min\n<b>Requested by:</b> %s",
+		"<u><b>| Mulai streaming</b></u>\n\n<b>Judul:</b> <a href='%s'>%s</a>\n<b>Durasi:</b> %s min\n<b>Req by:</b> %s",
 		escURL, escName, utils.SecToMin(saveCache.Duration), escUser,
 	)
 
@@ -334,7 +334,7 @@ func handleSingleTrack(c *td.Client, m *td.Message, updater *td.Message, song ut
 		escName := html.EscapeString(saveCache.Name)
 		escUser := html.EscapeString(saveCache.User)
 		queueInfo := fmt.Sprintf(
-			"<u><b>Added to queue: %d</b></u>\n\n<b>Title:</b> <a href='%s'>%s</a>\n\n<b>Duration:</b> %s min\n<b>Requested by:</b> %s",
+			"<u><b>Tambah ke antrian: %d</b></u>\n\n<b>Judul:</b> <a href='%s'>%s</a>\n<b>Durasi:</b> %s min\n<b>Req by:</b> %s",
 			qLen, escURL, escName, utils.SecToMin(saveCache.Duration), escUser,
 		)
 
@@ -364,7 +364,7 @@ func handleSingleTrack(c *td.Client, m *td.Message, updater *td.Message, song ut
 	escUsernp := html.EscapeString(saveCache.User)
 
 	nowPlaying := fmt.Sprintf(
-		"<u><b>| Started streaming</b></u>\n\n<b>Title:</b> <a href='%s'>%s</a>\n\n<b>Duration:</b> %s min\n<b>Requested by:</b> %s",
+		"<u><b>| Mulai streaming</b></u>\n\n<b>Judul:</b> <a href='%s'>%s</a>\n<b>Durasi:</b> %s min\n<b>Req by:</b> %s",
 		escURLnp, escNamenp, utils.SecToMin(song.Duration), escUsernp,
 	)
 
